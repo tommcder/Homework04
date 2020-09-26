b6a8ddd4ca1e20a7c12917578135485ed3d3fb26
tommcder <70983324+tommcder@users.noreply.github.com>
2020-09-25 21:08:48 -0400

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        System.out.println(" 1 + 2 + ... + " + n + " = " + Main.sum(n) + " = " + n + " * (" + n + " + 1) / 2");
-        System.out.println(" 1^2 + 2^2 + ... + " + n + "^2 = " + Main.squareSum(n) + " = " + n + " * (" + n + " + 1) * (2 * "+n+" + 1) / 6");
-        System.out.println(" 1^3 + 2^3 + ... + " + n + "^3 = " + Main.cubedSum(n) + " = " + n + " * (" + n + " + 1) * (2 * "+n+" + 1) / 6");
+        System.out.println("The numbers 1 - "+n+" will be summed, squared and summed, and lastly cubed and summed.");
+        System.out.println(" 1 + 2 + ... + " + n + " = " + Main.sum(n) + " = (" + n + " * (" + n + " + 1)) / 2");
+        System.out.println(" 1^2 + 2^2 + ... + " + n + "^2 = " + Main.squareSum(n) + " = (" + n + " * (" + n + " + 1) * (2 * "+n+" + 1)) / 6");
+        System.out.println(" 1^3 + 2^3 + ... + " + n + "^3 = " + Main.cubedSum(n) + " = (" + n + " ^ 2 * (" + n + " + 1) ^ 2) / 2");
     }
 
 
@@ -25,6 +26,6 @@ public class Main {
     public static double cubedSum(int m)
     {
         //int index = 0;
-        return ((Math.pow(m,2)*Math.pow((m+1), 2))/2);
+        return ((Math.pow(m,2)/2*Math.pow((m+1), 2))/2);
     }
 }
